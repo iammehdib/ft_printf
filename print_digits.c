76c321ae2a23fe75556a1cc441441a2038458fa9
Mehdi Buchet <mbuchet@student.42belgium.be>
2026-04-20 18:59:53 +0200

    fix(print_digits): fix print_hexadecimal

--- print_digits.c
+++ print_digits.c
@@ -6,7 +6,7 @@
 /*   By: mbuchet <mbuchet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/19 15:28:26 by mbuchet           #+#    #+#             */
-/*   Updated: 2026/04/19 19:12:24 by mbuchet          ###   ########.fr       */
+/*   Updated: 2026/04/20 18:58:01 by mbuchet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,17 +37,17 @@ void	print_number_unsigned(unsigned int number, size_t *str_lenght)
 	print_char((n_long % 10) + '0', str_lenght);
 }
 
-void	print_hexadecimal(int number, int is_uppercase, size_t *str_lenght)
+void	print_hexadecimal(unsigned int number, int is_uppercase, size_t *str_lenght)
 {
-	long	n_long;
-	char	*str;
+	unsigned long	n_long;
+	char		*str;
 
 	n_long = number;
 	if (is_uppercase == 1)
-		str = "123456789ABCDEF";
+		str = "0123456789ABCDEF";
 	else
-		str = "123456789abcdef";
+		str = "0123456789abcdef";
 	if (n_long >= 16)
-		print_number(n_long / 16, str_lenght);
+		print_hexadecimal(n_long / 16, is_uppercase, str_lenght);
 	print_char(str[n_long % 16], str_lenght);
 }
