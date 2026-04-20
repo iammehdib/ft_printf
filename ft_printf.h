1564e3de4baea329baba8f69345c29c93af9743b
Mehdi Buchet <mbuchet@student.42belgium.be>
2026-04-20 19:01:08 +0200

    refactor(ft_printf.h): replace int to unsigned for print_hexadecimal prototype

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: mbuchet <mbuchet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/19 19:05:07 by mbuchet           #+#    #+#             */
-/*   Updated: 2026/04/19 19:14:10 by mbuchet          ###   ########.fr       */
+/*   Updated: 2026/04/20 18:58:14 by mbuchet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	print_char(char c, size_t *str_lenght);
 void	print_string(char *str, size_t *str_lenght);
 void	print_number(int number, size_t *str_lenght);
 void	print_number_unsigned(unsigned int number, size_t *str_lenght);
-void	print_hexadecimal(int number, int is_uppercase, size_t *str_lenght);
+void	print_hexadecimal(unsigned int number, int is_uppercase, size_t *str_lenght);
 void	print_pointer(intptr_t pointer, size_t *str_lenght);
 
 #endif
